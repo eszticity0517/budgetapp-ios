87c87b4ce735b9c6d7c0aaaa887408e410c4d337
eszticity0517 <43317549+eszticity0517@users.noreply.github.com>
2021-01-20 14:23:51 +0100

    project creation

---
+++ budgetapp-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  budgetapp-ios
+//
+//  Created by Szabó Eszter on 2021. 01. 20..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ budgetapp-iosTests/budgetapp_iosTests.swift
@@ -0,0 +1,33 @@
+//
+//  budgetapp_iosTests.swift
+//  budgetapp-iosTests
+//
+//  Created by Szabó Eszter on 2021. 01. 20..
+//
+
+import XCTest
+@testable import budgetapp_ios
+
+class budgetapp_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
